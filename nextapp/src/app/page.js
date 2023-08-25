f56165028c1094a33b8bd067a90fa75de3588f98
KimGyeongwon <ruddnjs6651@gmail.com>
2023-08-25 21:57:35 +0900

    08/25 생활코딩Next Create까지

--- nextapp/src/app/page.js
+++ nextapp/src/app/page.js
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return <>Hello, NextJS!!!</>;
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB!
+      <br />
+      <Image src="/aibot.jpg" alt="aibot" width="300" height="300" />
+    </>
+  );
 }
---
+++ nextapp/src/app/read/[id]/page.js
@@ -0,0 +1,10 @@
+export default async function Read(props) {
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await res.json();
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
+}
